eda98a36647c2d493f41420e46b38e66a5bfe68c
Rafael Souza <rafael.souza@ahreas.com>
2024-05-19 08:50:31 -0300

    css mobile

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
+(function () {
+  window.addEventListener('scroll', function() {
+      let shadeOpacity = this.window.scrollY / 500;
+      let bgScale = (this.window.scrollY * .0004) + 1; // higher number for more zoom
+      let textMarginTop = (this.window.scrollY * .2) + 1; // Title speed
+
+      this.document.querySelector('.shade').style.opacity = shadeOpacity;
+      this.document.querySelector('.bg').style.transform = `scale(${bgScale})`;
+      this.document.querySelector('.text').style.marginTop = textMarginTop;
+      
+      let header = this.document.querySelector('header');
+      let menu = this.document.querySelector('menu');
+      if (window.scrollY >= (header.clientHeight - (header.clientHeight*5/100))) {
+          menu.classList.remove('fadeOutUp');
+          menu.classList.add('fadeInDown');
+      } else {
+          menu.classList.remove('fadeInDown');
+          menu.classList.add('fadeOutUp');
+      }
+  });
+}.call(this));
+
 const graphOptions = {
     orientation: 'vertical-reverse',
-    mode: 'compact'
+    mode: 'compact',
+    responsive: true
 };
 
 const commitWithoutDot = {
@@ -164,28 +187,6 @@ const branchConf = {
       }
     }
 };
-
-(function () {
-    window.addEventListener('scroll', function() {
-        let shadeOpacity = this.window.scrollY / 500;
-        let bgScale = (this.window.scrollY * .0004) + 1; // higher number for more zoom
-        let textMarginTop = (this.window.scrollY * .2) + 1; // Title speed
-
-        this.document.querySelector('.shade').style.opacity = shadeOpacity;
-        this.document.querySelector('.bg').style.transform = `scale(${bgScale})`;
-        this.document.querySelector('.text').style.marginTop = textMarginTop;
-        
-        let header = this.document.querySelector('header');
-        let menu = this.document.querySelector('menu');
-        if (window.scrollY >= (header.clientHeight - (header.clientHeight*5/100))) {
-            menu.classList.remove('fadeOutUp');
-            menu.classList.add('fadeInDown');
-        } else {
-            menu.classList.remove('fadeInDown');
-            menu.classList.add('fadeOutUp');
-        }
-    });
-}.call(this));
 
 window.onload = function(){
   drawGraphFeat();
